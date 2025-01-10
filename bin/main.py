36c30c06caad91b77e7109366db0de8e641c19ea
“scbarrera” <scbarrera@users.noreply.github.com>
2025-01-10 12:58:19 -0500

    partitioning contd.-> adding defined variables from extraction_attempt.py to main.py

--- bin/main.py
+++ bin/main.py
@@ -7,6 +7,11 @@ __author__ = "SC Barrera"
 from os.path import join
 #from grna_extraction.extraction_attempt import process_paired_read_file
 
+from Bio.Seq import reverse_complement
+
+import regex
+
+
 def main():
     print('running_main')
     csv = '409-4.csv'
@@ -16,6 +21,13 @@ def main():
     dir_read = join(dir_crispr, 'reads/')
     path1 = join(dir_read, '409-4_S1_L001_R1_001.fastq.gz')
     path2 = join(dir_read, '409-4_S1_L001_R2_001.fastq.gz')
+
+    forward_umi_reg = regex.compile('(?P<UMI>.{8})(?:CTTGGCTTTATATATCTTGTGG){s<=4}', flags=regex.BESTMATCH) 
+    protospacer_reg = regex.compile('(?:TATCTTGTGGAAAGGACGAAACACC){s<=4}(?P<protospacer>.{19,21})(?:GTTTAAGTACTCTGTGCTGGAAACAG){s<=4}', flags=regex.BESTMATCH)
+    back_umi_forward = 'gtgtgtcagttagggtgtggaa'.upper()
+    back_umi_rc = reverse_complement(back_umi_forward)
+    reverse_umi_reg = regex.compile(f'(?P<UMI>.{{8}})(?:{back_umi_rc}){{s<=4}}', flags=regex.BESTMATCH) 
+
     print(f'path1 = {path1}')
     print(f'path2 = {path2}')
     #process_paired_read_file(csv, path1, path2, forward_umi_reg, protospacer_reg, reverse_umi_reg)
