30ab361a2401aaee42db55490dd9829b1073599c
“scbarrera” <scbarrera@users.noreply.github.com>
2025-02-04 16:08:43 -0500

    added assert for dct keys in test file

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,9 +12,13 @@ def test_config():
     assert cfg.doc is not None 
     # assert not dct, f"Dictionary contains stuff & shouldn't:\n{dct}"
     assert dct, f"Dictionary is empty & should contain stuff: {dct}"
+    assert sorted(dct.keys()) == sorted(['title', 'owner', 'define_motifs'])
+    assert sorted(dct['define_motifs'].keys()) == sorted(['regex_flags', 'umi_pattern_forward', 'protospacer_pattern', 'back_umi_forward', 'umi_pattern_reverse'])
+
     
     print(cfg)
     print(dct)
+    print(dct['define_motifs'].keys())
     print("TEST PASSED")
 
 if __name__ == '__main__':
